694bef78cac1081332a345cccad478875d7c7f46
CoppoloCode <matthewcoppolo@yahoo.com>
2023-02-12 19:26:02 -0500

    Update server.js

--- ChessPlay/server.js
+++ ChessPlay/server.js
@@ -3,19 +3,6 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const mysql = require('mysql');
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'chessplay'
-});
-
-con.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
-
 
 app.set('view engine' , 'ejs');
 app.use(express.static('public'));
